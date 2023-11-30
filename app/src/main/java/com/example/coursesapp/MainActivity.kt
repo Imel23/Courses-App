3c4e630db2498dd4dfa319038f0fd60246d511a9
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-11-30 23:25:19 +0100

    Make a grid with two columns

--- app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.coursesapp
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.coursesapp.data.DataSource
 import com.example.coursesapp.model.Topic
 import com.example.coursesapp.ui.theme.CoursesAppTheme
 
@@ -29,13 +33,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CoursesAppTheme {
-
+                CoursesGrid(DataSource.topics)
             }
         }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun CoursesGrid(topicList: List<Topic>){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
+    ){
+        items(topicList){
+                topic ->
+            run {
+                TopicCard(
+                    topic = topic,
+                    modifier = Modifier
+                )
+            }
+        }
+
+    }
+}
+
+
 @Composable
 fun TopicCard(modifier: Modifier, topic: Topic){
     Card(modifier = modifier) {
