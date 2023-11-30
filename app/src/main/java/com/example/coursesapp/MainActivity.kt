a17c19738cc3279b11c2fd68c0f90d33d4b609c6
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-11-30 23:30:22 +0100

    Modify image content scale

--- app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -20,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -71,6 +73,8 @@ fun TopicCard(modifier: Modifier, topic: Topic){
                 modifier= Modifier
                     .height(68.dp)
                     .width(68.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
                 )
             Column(modifier=Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)) {
                 Text(
